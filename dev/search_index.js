22b6c66d347deaba5d79f81ecaf4c7d51c6cc257
Documenter.jl <documenter@juliadocs.github.io>
2025-02-03 10:57:39 +0000

    build based on 50283ae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QFiND","category":"page"},{"location":"#QFiND","page":"Home","title":"QFiND","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QFiND.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QFiND]","category":"page"}]
+}
