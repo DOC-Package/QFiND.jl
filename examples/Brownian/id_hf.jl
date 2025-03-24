0ef92fe45d015612cb3f4f3d355b1f0e27b37525
Hideaki Takahashi <hide.kyoto.1020@gmail.com>
2025-03-24 19:10:20 +0100

    debugging hightemp

--- examples/Brownian/id_hf.jl
+++ examples/Brownian/id_hf.jl
@@ -12,26 +12,18 @@ Temp = 300.0
 N_w = 4000
 T_c = 200.0
 N_t = 500
-eps = 5e-3
+eps = 1e-3
 
 sdens = BrownianSD(Ω, Γ, λ)
-sbeta = BosonicQNSD_HighT(sdens, Temp)
-bht = BrownianHighT(sdens, Temp; scale=icm2ifs)
-bcf = t -> sumexp(t, bht.expon, bht.coeff)
-
-#println("coeff", bht.coeff ./ icm2ifs^2.0)
-#println("expon", bht.expon ./ icm2ifs) 
-
-save_array_unite("bo_ht_hf.txt", bht.coeff ./ icm2ifs^2.0, bht.expon ./ icm2ifs)
-
-
+sbeta = BosonicQNSD(sdens, Temp)
+bcf = BosonicBCF(sdens, Temp, Ω_c)
 dataset = InitialData(DiscrID(), sbeta, bcf, Ω_min, Ω_max, T_c; n_freq=N_w, n_time=N_t)
 res = id_discr(dataset, eps)
 
 freq = res.freq
 coef = res.coef
 evaluate_error(freq, coef, bcf, T_c, N_t)
-plot_bcf(freq, coef, bcf, T_c, N_t, "id_ht.png")
+plot_bcf(freq, coef, bcf, T_c, N_t, "id_hf.png")
 
-save_array("omega_g_ht_hf.txt", freq, sqrt.(coef))
+save_array("omega_g_bo_hf.txt", freq, sqrt.(coef))
 
--- examples/save.jl
+++ examples/save.jl
@@ -27,6 +27,21 @@ function save_array(filename::String, array1::Vector{Complex{T}}, array2::Vector
     end
 end
 
+function save_array_unite(filename::String, array1::Vector{Complex{T}}, array2::Vector{Complex{T}}) where T
+    open(filename, "w") do io
+        write(io, " "^15 * "Coefficients" * " "^25 * "Exponents\n")
+        write(io, " "^10 * "Real" * " "^10 * "Imag" * " "^20 * "Real" * " "^10 * "Imag\n")
+        write(io, "-"^80 * "\n")  
+        
+        for (c1, c2) in zip(array1, array2)
+            write(io, @sprintf("%0.12e  %0.12e  %0.12e  %0.12e  %0.12e  %0.12e\n", 
+                               real(c1), imag(c1), real(c1), -imag(c1), real(c2), imag(c2)))
+            write(io, @sprintf("%0.12e  %0.12e  %0.12e  %0.12e  %0.12e  %0.12e\n", 
+                               real(c1), -imag(c1), real(c1), imag(c1), real(c2), -imag(c2)))
+        end
+    end
+end
+
 function save_array_union(filename::String, array1::Vector{Complex{T}}, array2::Vector{Complex{T}}) where T
     open(filename, "w") do io
         for (c1, c2) in zip(array1, array2)
